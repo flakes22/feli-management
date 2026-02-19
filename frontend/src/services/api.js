0c3f7807b38dd6e9bc6a4b424fa3580c3a042b8c
Mahek <mahek22d@gmail.com>
2026-02-19 15:20:57 +0530

    updated base url for deployment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5001/api",
+  baseURL: "https://feli-management.onrender.com",
 });
 
 API.interceptors.request.use((req) => {
